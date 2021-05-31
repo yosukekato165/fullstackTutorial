5b848fa535671f69de1954d5dae110763999084d
yosukekato165 <padyk165@gmail.com>
2021-05-31 21:50:42 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+import express from 'express';
+
+const main = async() => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send('hello');
+  })
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  })
+}
+
+
+
+
+main().catch(err => {
+  console.error(err);
+})
